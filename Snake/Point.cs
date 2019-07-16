3da66e92a3422a40bdc5a99b7946d0f79eebd510
limitoff <limit_vs@mail.ru>
2019-07-16 17:59:43 +0300

    Функции с урока

--- Snake/Point.cs
+++ Snake/Point.cs
@@ -6,23 +6,27 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class Point
-    {
-        public int x;
-        public int y;
-        public char sym;
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
 
-        public Point(int _x, int _y, char _sym)
-        {
-            x = _x;
-            y = _y;
-            sym = _sym;
-        }
+		public Point()
+		{
+		}
 
-        public void Draw()
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-    }
+		public Point(int _x, int _y, char _sym)
+		{
+			x = _x;
+			y = _y;
+			sym = _sym;
+		}
+
+		public void Draw()
+		{
+			Console.SetCursorPosition( x, y );
+			Console.Write( sym );			
+		}
+	}
 }
