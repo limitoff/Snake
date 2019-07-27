3586d641898e1859463996076ab497d7b7b981b2
limitoff <limit_vs@mail.ru>
2019-07-27 19:56:44 +0300

    Добавлена еда и Змейка теперь может есть её и расти.

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -13,7 +13,7 @@ namespace Snake
         {
             Console.SetBufferSize(80, 25);
             Console.CursorVisible = false; //убирает видимость курсора
-
+            // Отрисовка рамочки
             HorizontalLine topLine = new HorizontalLine(0, 78, 0, '-');
             HorizontalLine bottomLine = new HorizontalLine(0, 78, 24, '-');
             VerticalLine leftLine = new VerticalLine(0, 24, 0, '|');
@@ -22,21 +22,31 @@ namespace Snake
             bottomLine.DrawLine();
             leftLine.DrawLine();
             rightLine.DrawLine();
-
+            // Отрисовка точек
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.DrawLine();
-            
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.DrawPoint();
+
             while (true) //Бесконечный цикл, он означает, что код внутри цикла будет выполняться вечно.
             {
-                if (Console.KeyAvailable) //Проверка на то, была ли нажата какая-либо клавиша
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.DrawPoint();
+                }
+                else snake.MoveSnake();
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey(true);
                     snake.HendleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.MoveSnake();
             }
+
         }
     }
 }
